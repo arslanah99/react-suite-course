8189da8e9be0a79b24615e1ad5d47b3a2dd3703f
arslanah99 <42077616+arslanah99@users.noreply.github.com>
2022-06-26 20:14:38 -0600

    initial commit

---
+++ rsuitecourse/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from "rsuite"
+// import default style
+import 'rsuite/dist/rsuite.min.css'
+function App() {
+  return (
+    <div className="App">
+    <Button>Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
