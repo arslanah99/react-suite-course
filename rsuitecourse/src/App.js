4d7de7ba66eb8d3d578a7ddb7eb955ff0965a1b5
arslanah99 <42077616+arslanah99@users.noreply.github.com>
2022-07-03 14:56:14 -0600

    compelted navbar

--- rsuitecourse/src/App.js
+++ rsuitecourse/src/App.js
@@ -1,27 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button, ButtonGroup, ButtonToolbar, CustomProvider, IconButton} from "rsuite"
+import {
+  Button,
+  ButtonGroup,
+  ButtonToolbar,
+  CustomProvider,
+  IconButton,
+  Navbar,
+  Nav,
+} from 'rsuite';
 // import default style
-import 'rsuite/dist/rsuite.min.css'
-import {Visible, Unvisible} from '@rsuite/icons';
-import { useEffect, useState } from 'react';
+import 'rsuite/dist/rsuite.min.css';
+import {Visible, Unvisible, Others,  ArrowUp, TextImage, Review} from '@rsuite/icons';
+import {useEffect, useState} from 'react';
 
 function App() {
-  const [toggleMode, setToggleMode] = useState(localStorage.getItem("toggle-mode") === 'true')
+  const [toggleMode, setToggleMode] = useState(
+    localStorage.getItem('toggle-mode') === 'true'
+  );
   useEffect(() => {
-    localStorage.setItem("toggle-mode", toggleMode)
-  }, [toggleMode])
+    localStorage.setItem('toggle-mode', toggleMode);
+  }, [toggleMode]);
   const toggleModeFunc = () => {
-    setToggleMode(!toggleMode)
-  }
+    setToggleMode(!toggleMode);
+  };
   return (
-    <CustomProvider theme={toggleMode ? "dark" : "light"}>
-    <div className="App">
-    <ButtonToolbar>
-      <IconButton onClick={toggleModeFunc} color="red" appearance='ghost' size="lg" icon={toggleMode ? <Visible /> : <Unvisible />} />
-    
-    </ButtonToolbar>
-    </div>
+    <CustomProvider theme={toggleMode ? 'dark' : 'light'}>
+      <Navbar>
+        <Navbar.Brand>THE GOAT COURSE</Navbar.Brand>
+        <Nav>
+          <Nav.Item icon={<ArrowUp />}>Home</Nav.Item>
+          <Nav.Item icon={<Others />}>Modal</Nav.Item>
+          <Nav.Menu title="About">
+            <Nav.Item icon={<TextImage />}>Home</Nav.Item>
+            <Nav.Item icon={<Review />}>Modal</Nav.Item>
+          </Nav.Menu>
+        </Nav>
+        <Nav pullRight>
+          <Nav.Item>
+            <ButtonToolbar>
+              <IconButton
+                onClick={toggleModeFunc}
+                color="red"
+                appearance="ghost"
+                size="lg"
+                icon={toggleMode ? <Visible /> : <Unvisible />}
+              />
+            </ButtonToolbar>
+          </Nav.Item>
+        </Nav>
+      </Navbar>
     </CustomProvider>
   );
 }
