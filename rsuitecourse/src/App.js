48d3341ecdaf9ae7363c4dcefbffa5e2a176a880
arslanah99 <42077616+arslanah99@users.noreply.github.com>
2022-07-03 12:41:17 -0600

    created button

--- rsuitecourse/src/App.js
+++ rsuitecourse/src/App.js
@@ -1,12 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button} from "rsuite"
+import {Button, ButtonGroup, ButtonToolbar, IconButton} from "rsuite"
 // import default style
 import 'rsuite/dist/rsuite.min.css'
+import {Gear, Message} from '@rsuite/icons';
+
 function App() {
   return (
     <div className="App">
-    <Button>Hello World</Button>
+    <Button appearance='subtle'>Hello World</Button>
+    <ButtonToolbar>
+      <Button color="red" appearance='ghost' size="lg">Large</Button>
+      <Button>md</Button>
+      <Button size="sm">small</Button>
+      <Button color="yellow" appearance='link' size="xs">xs</Button>
+    </ButtonToolbar>
+    <ButtonToolbar>
+      <IconButton icon={<Gear />}/>
+      <IconButton icon={<Message />} color="orange" appearance='primary' placement="right" circle>Message</IconButton>
+
+    </ButtonToolbar>
+    <Button block disabled loading appearance="primary">BUTTON</Button>
+    <ButtonToolbar>
+      <ButtonGroup>
+        <Button>#1</Button>
+        <Button>#2</Button>
+        <Button>#3</Button>
+        <Button>#4</Button>
+      </ButtonGroup>
+      <ButtonGroup>
+        <Button appearance='primary'>#1</Button>
+        <Button appearance='primary'>#2</Button>
+        <Button appearance='primary'>#3</Button>
+        <Button appearance='primary'>#4</Button>
+      </ButtonGroup>
+      <ButtonGroup>
+        <Button appearance='ghost'>#69</Button>
+      </ButtonGroup>
+    </ButtonToolbar>
     </div>
   );
 }
