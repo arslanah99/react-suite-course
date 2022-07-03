a423e3c1d126ad2ca493f7b26dda862827b3dd19
arslanah99 <42077616+arslanah99@users.noreply.github.com>
2022-07-03 13:30:23 -0600

    created light more and dark mode

--- rsuitecourse/src/App.js
+++ rsuitecourse/src/App.js
@@ -1,44 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button, ButtonGroup, ButtonToolbar, IconButton} from "rsuite"
+import {Button, ButtonGroup, ButtonToolbar, CustomProvider, IconButton} from "rsuite"
 // import default style
 import 'rsuite/dist/rsuite.min.css'
-import {Gear, Message} from '@rsuite/icons';
+import {Visible, Unvisible} from '@rsuite/icons';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [toggleMode, setToggleMode] = useState(localStorage.getItem("toggle-mode") === 'true')
+  useEffect(() => {
+    localStorage.setItem("toggle-mode", toggleMode)
+  }, [toggleMode])
+  const toggleModeFunc = () => {
+    setToggleMode(!toggleMode)
+  }
   return (
+    <CustomProvider theme={toggleMode ? "dark" : "light"}>
     <div className="App">
-    <Button appearance='subtle'>Hello World</Button>
     <ButtonToolbar>
-      <Button color="red" appearance='ghost' size="lg">Large</Button>
-      <Button>md</Button>
-      <Button size="sm">small</Button>
-      <Button color="yellow" appearance='link' size="xs">xs</Button>
-    </ButtonToolbar>
-    <ButtonToolbar>
-      <IconButton icon={<Gear />}/>
-      <IconButton icon={<Message />} color="orange" appearance='primary' placement="right" circle>Message</IconButton>
-
-    </ButtonToolbar>
-    <Button block disabled loading appearance="primary">BUTTON</Button>
-    <ButtonToolbar>
-      <ButtonGroup>
-        <Button>#1</Button>
-        <Button>#2</Button>
-        <Button>#3</Button>
-        <Button>#4</Button>
-      </ButtonGroup>
-      <ButtonGroup>
-        <Button appearance='primary'>#1</Button>
-        <Button appearance='primary'>#2</Button>
-        <Button appearance='primary'>#3</Button>
-        <Button appearance='primary'>#4</Button>
-      </ButtonGroup>
-      <ButtonGroup>
-        <Button appearance='ghost'>#69</Button>
-      </ButtonGroup>
+      <IconButton onClick={toggleModeFunc} color="red" appearance='ghost' size="lg" icon={toggleMode ? <Visible /> : <Unvisible />} />
+    
     </ButtonToolbar>
     </div>
+    </CustomProvider>
   );
 }
 
